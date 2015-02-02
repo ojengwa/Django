fd4944aaeef4bf32f0fb044e5f50c5ea266da5fa
Bernard Ojengwa <bernard@Bernards-MacBook-Pro.local>
2015-02-02 12:05:44 +0100

    FIX TO DELETE

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from selenium import webdriver
 
 # Create your tests here.
+
+class SimpleTest(TestCase):
+    """docstring for SimpleTest"""
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def test_addition(self):
+        self.assertEqual(1+2, 3)
+
+    def test_web(self):
+        self.browser.get('http://google.com')
+
+        assert 'Django' in self.browser.title
+
+    def tearDown(self):
+        self.browser.quit()
+
+        return 0

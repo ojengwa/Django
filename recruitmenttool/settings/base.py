fd5896d0f9ab984766e006a97983964c83b38931
ifedapo <ifedapoolarewaju@gmail.com>
2015-02-02 17:44:00 +0100

    plugged REST framework setup

--- recruitmenttool/settings/base.py
+++ recruitmenttool/settings/base.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'reports',
     'applicant'
 )
@@ -78,3 +79,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}

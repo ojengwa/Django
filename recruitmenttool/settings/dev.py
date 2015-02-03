b1693db060d80e5a5f782e2c5dc8b48f6d9668f6
gbolly <gbolahan.okerayi@andela.co>
2015-02-03 17:14:01 +0100

    Changed database to postgres

--- recruitmenttool/settings/dev.py
+++ recruitmenttool/settings/dev.py
@@ -2,7 +2,11 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'saint han',
+        'HOST': 'localhost',
+        'PORT': 5433,
     }
 }

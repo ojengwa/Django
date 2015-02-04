76207f70da494a9c87530db75f219d5eaae296a1
gbolly <gbolahan.okerayi@andela.co>
2015-02-04 15:52:32 +0100

    Created appplication app and model

--- recruitmenttool/settings/dev.py
+++ recruitmenttool/settings/dev.py
@@ -1,20 +1,12 @@
 from .base import *
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'circle_test',
-#         'USER': 'ubuntu',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': 5432,
-#         'TEST_NAME': 'circle_test',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'postgres',
+        'PASSWORD': 'saint han',
+        'HOST': 'localhost',
+        'PORT': 5433,
     }
 }

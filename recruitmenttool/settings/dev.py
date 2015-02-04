e747606b402fa933aff7615e9c931b76d71d2a79
Bernard Ojengwa <bernardojengwa@gmail.com>
2015-02-04 01:28:42 +0100

    Added test runner

--- recruitmenttool/settings/dev.py
+++ recruitmenttool/settings/dev.py
@@ -1,13 +1,20 @@
 from .base import *
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'circle_test',
+#         'USER': 'ubuntu',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#         'TEST_NAME': 'circle_test',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydb',
-        'USER': 'postgres',
-        'PASSWORD': '[]',
-        'HOST': 'localhost',
-        'PORT': 5433,
-        'TEST_NAME': 'circle_test',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }

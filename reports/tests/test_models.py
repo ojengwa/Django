83bf4402639716d826d31ed2fc9a189b09761672
gbolly <gbolahan.okerayi@andela.co>
2015-02-04 08:27:50 +0100

    Test for report model

--- reports/tests/test_models.py
+++ reports/tests/test_models.py
@@ -3,11 +3,20 @@ from django.test import TestCase
 from reports.models import Report
 
 class ReportModelTest(TestCase):
+	def test_model_fields(self):
+        """
+        test that model contains certain fields
+        """
+        fields = {field.name: field for field in Report._meta.fields}
+
+        self.assertTrue(fields.has_key('title'))
+        self.assertTrue(fields.has_key('content'))
+        self.assertTrue(fields.has_key('author'))
+        self.assertTrue(fields.has_key('applicant'))
+        self.assertTrue(fields.has_key('updated'))
+        self.assertTrue(fields.has_key('created_at'))
+        self.assertTrue(fields.has_key('is_public'))
 
 	def test_string_representation(self):
-		report = Report(report_title="My Title")
+		report = Report(title="My Title")
 		self.assertEqual(str(report), report.title)
-
-	def test_string_representation(self):
-		report = Report(report_title="My Title")
-		self.assertEqual(str(report), report.content)
